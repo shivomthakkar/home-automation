cf775b59ead531c06cba739070cdbcfca8a7ecfb
Shivom Thakkar <shivomthakkar15@gmail.com>
2021-03-20 02:00:54 +0530

    Init commit

--- app/ota_updater.py
+++ app/ota_updater.py
@@ -1,20 +1,13 @@
-import os, gc
-from .httpclient import HttpClient
+import os
+import usocket
 
 class OTAUpdater:
-    """
-    A class to update your MicroController with the latest version from a GitHub tagged release,
-    optimized for low power usage.
-    """
 
-    def __init__(self, github_repo, github_src_dir='', module='', main_dir='main', new_version_dir='next', secrets_file=None, headers={}):
+    def __init__(self, github_repo, module='', main_dir='src', headers={}):
         self.http_client = HttpClient(headers=headers)
-        self.github_repo = github_repo.rstrip('/').replace('https://github.com/', '')
-        self.github_src_dir = '' if len(github_src_dir) < 1 else github_src_dir.rstrip('/') + '/'
-        self.module = module.rstrip('/')
+        self.github_repo = github_repo.rstrip('/').replace('https://github.com', 'https://api.github.com/repos')
         self.main_dir = main_dir
-        self.new_version_dir = new_version_dir
-        self.secrets_file = secrets_file
+        self.module = module.rstrip('/')
 
     def __del__(self):
         self.http_client = None
@@ -27,7 +20,6 @@ class OTAUpdater:
         If a newer version is available, the file 'next/.version' will be created 
         and you need to call machine.reset(). A reset is needed as the installation process 
         takes up a lot of memory (mostly due to the http stack)
-
         Returns
         -------
             bool: true if a new version is available, false otherwise
@@ -46,7 +38,6 @@ class OTAUpdater:
         
         This method, which should be called first thing after booting, will check if the 
         next/.version' file exists. 
-
         - If yes, it initializes the WIFI connection, downloads the latest version and installs it
         - If no, the WIFI connection is not initialized as no new known version is available
         """
@@ -68,7 +59,6 @@ class OTAUpdater:
         This method expects an active internet connection and allows you to decide yourself
         if you want to install the latest version. It is necessary to run it directly after boot 
         (for memory reasons) and you need to restart the microcontroller if a new version is found.
-
         Returns
         -------
             bool: true if a new version is available, false otherwise
@@ -239,3 +229,105 @@ class OTAUpdater:
 
     def modulepath(self, path):
         return self.module + '/' + path if self.module else path
+
+class HttpClient:
+
+    def __init__(self, headers={}):
+        self._headers = headers
+
+    def request(self, method, url, data=None, json=None, headers={}, stream=None):
+        def _write_headers(sock, _headers):
+            for k in _headers:
+                sock.write(b'{}: {}\r\n'.format(k, _headers[k]))
+
+        try:
+            proto, dummy, host, path = url.split('/', 3)
+        except ValueError:
+            proto, dummy, host = url.split('/', 2)
+            path = ''
+        if proto == 'http:':
+            port = 80
+        elif proto == 'https:':
+            import ussl
+            port = 443
+        else:
+            raise ValueError('Unsupported protocol: ' + proto)
+
+        if ':' in host:
+            host, port = host.split(':', 1)
+            port = int(port)
+
+        ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
+        ai = ai[0]
+
+        s = usocket.socket(ai[0], ai[1], ai[2])
+        try:
+            s.connect(ai[-1])
+            if proto == 'https:':
+                s = ussl.wrap_socket(s, server_hostname=host)
+            s.write(b'%s /%s HTTP/1.0\r\n' % (method, path))
+            if not 'Host' in headers:
+                s.write(b'Host: %s\r\n' % host)
+            # Iterate over keys to avoid tuple alloc
+            _write_headers(s, self._headers)
+            _write_headers(s, headers)
+
+            # add user agent
+            s.write('User-Agent')
+            s.write(b': ')
+            s.write('MicroPython OTAUpdater')
+            s.write(b'\r\n')
+            if json is not None:
+                assert data is None
+                import ujson
+                data = ujson.dumps(json)
+                s.write(b'Content-Type: application/json\r\n')
+            if data:
+                s.write(b'Content-Length: %d\r\n' % len(data))
+            s.write(b'\r\n')
+            if data:
+                s.write(data)
+
+            l = s.readline()
+            # print(l)
+            l = l.split(None, 2)
+            status = int(l[1])
+            reason = ''
+            if len(l) > 2:
+                reason = l[2].rstrip()
+            while True:
+                l = s.readline()
+                if not l or l == b'\r\n':
+                    break
+                # print(l)
+                if l.startswith(b'Transfer-Encoding:'):
+                    if b'chunked' in l:
+                        raise ValueError('Unsupported ' + l)
+                elif l.startswith(b'Location:') and not 200 <= status <= 299:
+                    raise NotImplementedError('Redirects not yet supported')
+        except OSError:
+            s.close()
+            raise
+
+        resp = Response(s)
+        resp.status_code = status
+        resp.reason = reason
+        return resp
+
+    def head(self, url, **kw):
+        return self.request('HEAD', url, **kw)
+
+    def get(self, url, **kw):
+        return self.request('GET', url, **kw)
+
+    def post(self, url, **kw):
+        return self.request('POST', url, **kw)
+
+    def put(self, url, **kw):
+        return self.request('PUT', url, **kw)
+
+    def patch(self, url, **kw):
+        return self.request('PATCH', url, **kw)
+
+    def delete(self, url, **kw):
+        return self.request('DELETE', url, **kw)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 
 def connectToWifiAndUpdate():
-    import time, machine, network, gc, app.secrets as secrets
+    import time, machine, network, gc
+    from app.secrets import Secrets
     time.sleep(1)
     print('Memory free', gc.mem_free())
 
@@ -9,13 +10,16 @@ def connectToWifiAndUpdate():
 
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
-        print('connecting to network...')
+        print('Connecting to network...')
+        secrets = Secrets()
         sta_if.active(True)
         sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
-    otaUpdater = OTAUpdater('https://github.com/rdehuyss/micropython-ota-updater', main_dir='app', secrets_file="secrets.py")
+    # otaUpdater = OTAUpdater('https://github.com/shivomthakkar/micropython-ota-updater', main_dir='app', secrets_file="secrets.py")
+    token='aa517681738b4971cc55b7d9e8967780ff1f443b'
+    otaUpdater = OTAUpdater('https://github.com/shivomthakkar/micropython-ota-updater', main_dir="app", headers={'Authorization': 'token {}'.format(token)})
     hasUpdated = otaUpdater.install_update_if_available()
     if hasUpdated:
         machine.reset()
